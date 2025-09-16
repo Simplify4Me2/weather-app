7182af1a5e3185168dab0d3b5e5463e4b3d60889
Simon <simon.vereecke@live.com>
2025-09-16 22:03:28 +0200

    Add page title

--- src/app/app.ts
+++ src/app/app.ts
@@ -17,18 +17,20 @@ import { HeaderComponent } from './components/header/header';
     HourlyForecastComponent,
     HeaderComponent
   ],
-  // template: `
-  //   <div class="min-h-screen bg-neutral-900">
-  //     <div class="max-w-2xl mx-auto">
-  //       <app-search-bar />
-  //       <app-current-weather />
-  //       <app-weather-details />
-  //       <app-daily-forecast />
-  //       <app-hourly-forecast />
-  //     </div>
-  //   </div>
-  // `,
-  templateUrl: './app-component.html',
+  template: `
+    <div class="min-h-screen bg-neutral-900">
+      <div class="max-w-2xl mx-auto">
+        <app-header />
+        <h1 class="font-bricolage font-bold text-5xl text-white text-center mx-9 my-8">How's the sky looking today?</h1>
+        <app-search-bar />
+        <app-current-weather />
+        <app-weather-details />
+        <app-daily-forecast />
+        <app-hourly-forecast />
+      </div>
+    </div>
+  `,
+  // templateUrl: './app-component.html',
   styles: [`
     :host {
       display: block;
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -5,21 +5,12 @@ import { Component } from '@angular/core';
     standalone: true,
     template: `
     <header class="flex flex-row p-4">
-            <img src="assets/images/logo.svg" alt="Weather App Logo" class="h-7 mt-1">
-            <button class="flex flex-row items-center ml-auto px-2 rounded-lg bg-neutral-800">
-                <img src="assets/images/icon-units.svg" alt="Units">
-                <span class="text-white text-sm mx-1 font-dm">Units</span>
-                <img src="assets/images/icon-dropdown.svg" alt="Dropdown">
-            </button>
-        <!-- <div class="relative inline-block text-left">
-            <div>
-                <button type="button" class="inline-flex justify-center w-full rounded-md border bg-black border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" id="options-menu" aria-expanded="true" aria-haspopup="true">
-                    Units
-                    <img src="assets/images/icon-units.svg" alt="Units" class="w-5 h-5 ml-2">
-                </button>
-            </div>
-           
-        </div> -->
+        <img src="assets/images/logo.svg" alt="Weather App Logo" class="h-7 mt-1">
+        <button class="flex flex-row items-center ml-auto px-2 rounded-lg bg-neutral-800">
+            <img src="assets/images/icon-units.svg" alt="Units">
+            <span class="text-white text-sm mx-1 font-dm">Units</span>
+            <img src="assets/images/icon-dropdown.svg" alt="Dropdown">
+        </button>
     </header>
   `,
     styles: [`
