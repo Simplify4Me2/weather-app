485681422202b0526031e2c7614e8dbe3c42f68f
Simon <simon.vereecke@live.com>
2025-09-16 22:23:53 +0200

    update search bar button

--- src/app/components/search-bar/search-bar.ts
+++ src/app/components/search-bar/search-bar.ts
@@ -6,28 +6,23 @@ import { NgOptimizedImage } from '@angular/common';
   standalone: true,
   imports: [NgOptimizedImage],
   template: `
-    <div class="flex items-center justify-between p-4">
+    <div class="flex flex-col  p-4">
       <div class="relative flex-1 max-w-md">
         <input
           type="text"
           placeholder="Search for a place..."
-          class="w-full px-4 py-2 pl-10 rounded-lg bg-blue-950 text-white placeholder-gray-400"
+          class="w-full px-4 pl-15 rounded-lg bg-neutral-800 h-14 font-dm text-white text-xl placeholder-neutral-200"
         />
         <img
           ngSrc="assets/images/icon-search.svg"
           width="20"
           height="20"
           alt="Search"
-          class="absolute left-3 top-1/2 transform -translate-y-1/2"
+          class="absolute left-6 top-1/2 transform -translate-y-1/2"
         />
       </div>
-      <button class="ml-4 p-2 rounded-lg bg-blue-950">
-        <img
-          ngSrc="assets/images/icon-units.svg"
-          width="24"
-          height="24"
-          alt="Toggle units"
-        />
+      <button class="p-2 mt-3 h-14 rounded-lg bg-blue-500 text-xl font-dm text-neutral-0">
+        Search
       </button>
     </div>
   `,
